3805be897fe1c650ddd3dd972e9d846a28f8ef55
guerrerocing <guerrerocing@outlook.com>
2024-05-25 18:35:04 -0600

    feat: Dashboard implemented on App * some style changes added * some changes added to App - Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import "./App.css";
 
+import Dashboard from "./views/Dashboard";
+
 function App() {
   return (
     <>
-      <h1>Vite + React</h1>
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Dashboard />
     </>
   );
 }
